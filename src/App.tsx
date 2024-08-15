4501a8a65c998406dc5c00d3e3604a53c0c9114d
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-08-15 20:21:10 +0300

    Coming Soon Page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+// Assets
+import ButtonIMG from "@/assets/images/btn.webp";
+import BackgroundLeft from "@/assets/images/background/left.webp";
+import BackgroundRight from "@/assets/images/background/right.webp";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className={`box`}>
+        <div className={`box-left ${isOpen ? "move-left" : ""}`}>
+          <img src={BackgroundLeft} alt="Left Background Image" />
+          <div className="wrap">
+            <span className="left">Vincenzo</span>
+          </div>
+        </div>
+
+        <div className={`box-right ${isOpen ? "move-right" : ""}`}>
+          <img src={BackgroundRight} alt="Right Background Image" />
+          <div className="wrap">
+            <span className="right">Balenti</span>
+          </div>
+        </div>
+
+        <div className={!isOpen ? "box-open" : "hidden"}>
+          <div className="btn-open" onClick={() => setIsOpen(!isOpen)}>
+            <img src={ButtonIMG} alt="Button Image" />
+          </div>
+          <div className="btn-shade"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <main>
+        <div className="bg-cover"></div>
+
+        <section className="wrapper">
+          <div className="text-style title">
+            <div className="top">Vincenzo Balenti</div>
+            <div className="bottom" aria-hidden="true">
+              Vincenzo Balenti
+            </div>
+          </div>
+
+          <div className="text-style content">
+            <div className="top">Coming Soon</div>
+            <div className="bottom" aria-hidden="true">
+              Coming Soon
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
